520fcb95fa02b5d7eafc91de812aa56dca6f959a
Hyunseop Han <Hsqb@users.noreply.github.com>
2018-11-12 14:00:44 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import pull from "pull-stream";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
+<h1>Hello Pull-Stream!</h1>
 <div>
   Look
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
